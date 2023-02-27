d2c4af78cc740920c7493c15d559531e411cafa1
Regina <rmyachina@mail.ru>
2023-02-27 17:39:06 +0300

    close popup added

---
+++ js/close popup.js
@@ -0,0 +1,6 @@
+let popup = document.querySelector('.Pop-up');
+let buttonClose = popup.querySelector('.button-close');
+buttonClose.addEventListener('click', function() {
+    popup.classList.add('_hidden');
+})
+
